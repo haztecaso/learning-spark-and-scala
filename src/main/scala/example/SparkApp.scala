22bd9bf4435fe9903fcb118b52e4c978ccfcb0a6
haztecaso <adrian.lattes@hablapps.com>
2024-11-21 17:48:58 +0100

    starting to test spark wide transformations with bike dataset

--- src/main/scala/example/SparkApp.scala
+++ src/main/scala/example/SparkApp.scala
@@ -1,18 +1,31 @@
-package example
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col}
 
 object SparkApp {
   def main(args: Array[String]): Unit = {
-    println("Inicializando sesión de spark")
     val spark = SparkSession.builder
       .appName("Simple Application")
       .master("local[1]")
       .getOrCreate()
+    
+    spark.conf.set("spark.sql.shuffle.partitions", "2")
 
-    val data = spark.read.textFile("readme.md")
-    val numAs = data.filter(col("value").contains("a")).count()
-    println(s"resultados:\n $numAs")
+    val data = spark
+      .read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("data/bike-data/201508_trip_data.csv")
+      .repartition(2)
+
+    println(s"Columnas del dataframe: ${data.columns.mkString(", ")}")
+
+    println(s"número de entradas: ${data.count()}")
+
+    data
+      .groupBy(col("Start Date"))
+      .count()
+      .show()
+
     spark.stop()
   }
 }
